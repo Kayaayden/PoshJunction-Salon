a7c82da5abd7669b71299d9d7e4a7f04df48a72b
Aidan Kayanja <kayaayden@gmail.com>
2025-03-19 19:04:08 +0300

    testimonial Added

--- js/main.js
+++ js/main.js
@@ -1,31 +1,56 @@
-
 // NavBar Menu
-const navTriggerBtn = document.querySelector('#new-trigger-btn');
-const navMenu = document.querySelector('#nav-menu');
+const navTriggerBtn = document.querySelector("#new-trigger-btn");
+const navMenu = document.querySelector("#nav-menu");
 
-// EventListener 
-navTriggerBtn.addEventListener('click', () => {navMenu.classList.toggle('nav-is-open')});
+// EventListener
+navTriggerBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-is-open");
+});
 
-//Swiper
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    pagination: {
-        el:'.swiper-pagination',
-        clickable: true,
+//Swiper -1 offer
+const swiper1 = new Swiper(".swiper-1", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination-1",
+    clickable: true,
+  },
+
+  // breakpoints
+  slidesPerView: 2,
+  spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
     },
-
-    // breakpoints
-    slidesPerView: 2,
-    spaceBetween: 20,
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-        },
-        960: {
-            slidePerView: 2,
-        },
-        1200: {
-            slidesperView: 2,
-        },
+    960: {
+      slidePerView: 2,
     },
+    1200: {
+      slidesperView: 2,
+    },
+  },
+});
+
+// Swipper -2 testimonial
+const swiper2 = new Swiper(".swiper-2", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination-2",
+    clickable: true,
+  },
+
+  //   breakpoints
+  slidesPerView: 3,
+  spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    960: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,40 @@
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    container:{
+    container: {
       padding: {
-        DEFAULT: '15px',
-
+        DEFAULT: "15px",
       },
     },
     screens: {
-      sm :'640px',
-      md :'768px',
-      lg :'960px',
-      xl :'1200px',
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
     },
-    fontFamily:{
-      primary: 'Poppins',
-      secondary: 'Open Sans',
-
+    fontFamily: {
+      primary: "Poppins",
+      secondary: "Open Sans",
     },
-    backgroundImage:{
-      hero: 'url(/assets/hero/hero-1.png)',
-      grid: 'url(/assets/grid.png)',
+    backgroundImage: {
+      hero: "url(/assets/hero/hero-1.png)",
+      grid: "url(/assets/grid.png)",
+      services: "url(/assets/Services.jpg)",
     },
     extend: {
       colors: {
         primary: {
-          DEFAULT:'#292f36',
-          hover: '#343e4a',
-
+          DEFAULT: "#292f36",
+          hover: "#343e4a",
         },
-        secondary:'#4d5053',
+        secondary: "#4d5053",
         accent: {
-          DEFAULT:'#00a3b0',
-          secondary: '#b4cdd6',
-          hover: '#29b2bd',
-          
+          DEFAULT: "#00a3b0",
+          secondary: "#b4cdd6",
+          hover: "#29b2bd",
         },
-
       },
     },
   },
   plugins: [],
-}
+};
